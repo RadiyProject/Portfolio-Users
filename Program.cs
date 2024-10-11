6c119998728aa2f4b628249cb6856d618e7970ce
RadiyX <sigor00@mail.ru>
2024-10-11 13:04:33 +0700

    Migrating added

--- Program.cs
+++ Program.cs
@@ -24,6 +24,12 @@ builder.Host.UseSerilog((context, configuration) =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
